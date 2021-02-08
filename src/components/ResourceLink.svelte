2a90d2ca5583a71420822b41f2cc7dad6afc4c26
Angelly D <angellydejesus@gmail.com>
2021-02-07 18:30:29 -0600

    feat(ResourceLinks): refactor to list
    Signed-off-by: Angelly D <angellydejesus@gmail.com>

--- src/components/ResourceLink.svelte
+++ src/components/ResourceLink.svelte
@@ -1,23 +1,19 @@
 <script>
-  export let author
-  export let authorImage
-  export let authorImageAlt
-  export let date
-  export let image
-  export let imageAlt
-  export let timeToConsume
-  export let type
-  export let title
-  export let summary
+  export let author;
+  export let authorImage;
+  export let authorImageAlt;
+  export let date;
+  export let image;
+  export let imageAlt;
+  export let timeToConsume;
+  export let type;
+  export let title;
+  export let summary;
 </script>
 
 <div class="flex flex-col rounded-lg shadow-lg overflow-hidden">
   <div class="flex-shrink-0">
-    <img
-      class="h-48 w-full object-cover"
-      src="{image}"
-      alt="{imageAlt}"
-    />
+    <img class="h-48 w-full object-cover" src={image} alt={imageAlt} />
   </div>
   <div class="flex-1 bg-white p-6 flex flex-col justify-between">
     <div class="flex-1">
@@ -39,8 +35,8 @@
           <span class="sr-only">{author}</span>
           <img
             class="h-10 w-10 rounded-full"
-            src="{authorImage}"
-            alt="{authorImageAlt}"
+            src={authorImage}
+            alt={authorImageAlt}
           />
         </a>
       </div>
--- src/components/ResourceLinks.svelte
+++ src/components/ResourceLinks.svelte
@@ -1,5 +1,47 @@
 <script>
   import ResourceLink from "./ResourceLink.svelte";
+
+  let resources = [
+    {
+      type: "Article",
+      title: "Boost your conversion rate",
+      summary:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto accusantium praesentium eius, ut atque fuga culpa, similique sequi cum eos quis dolorum.",
+      author: "Roel Aufderehar",
+      authorImage:
+        "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixqx=QWrkbJ5Lr0&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+      date: "Mar 16, 2020",
+      timeToConsume: "6 min read",
+      image:
+        "https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-1.2.1&ixqx=QWrkbJ5Lr0&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80",
+    },
+    {
+      type: "Video",
+      title: "How to use search engine optimization to drive sales",
+      summary:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit facilis asperiores porro quaerat doloribus, eveniet dolore. Adipisci tempora aut inventore optio animi., tempore temporibus quo laudantium.",
+      author: "Brenna Goyette",
+      authorImage:
+        "https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixqx=QWrkbJ5Lr0&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+      date: "Mar 16, 2020",
+      timeToConsume: "6 min read",
+      image:
+        "https://images.unsplash.com/photo-1547586696-ea22b4d4235d?ixlib=rb-1.2.1&ixqx=QWrkbJ5Lr0&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80",
+    },
+    {
+      type: "Case Study",
+      title: "Improve your customer experience",
+      summary:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint harum rerum voluptatem quo recusandae magni placeat saepe molestiae, sed excepturi cumque corporis perferendis hic.",
+      author: "Daniela Metz",
+      authorImage:
+        "https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixqx=QWrkbJ5Lr0&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+      date: "Jun 4, 2020",
+      timeToConsume: "11 min read",
+      image:
+        "https://images.unsplash.com/photo-1492724441997-5dc865305da7?ixlib=rb-1.2.1&ixqx=QWrkbJ5Lr0&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80",
+    },
+  ];
 </script>
 
 <div class="relative bg-gray-50 py-16 sm:py-24 lg:py-32">
@@ -26,36 +68,11 @@
     <div
       class="mt-12 mx-auto max-w-md px-4 grid gap-8 sm:max-w-lg sm:px-6 lg:px-8 lg:grid-cols-3 lg:max-w-7xl"
     >
-      <ResourceLink
-        type="Article"
-        title="Boost your conversion rate"
-        summary="Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto accusantium praesentium eius, ut atque fuga culpa, similique sequi cum eos quis dolorum."
-        author="Roel Aufderehar"
-        authorImage="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixqx=QWrkbJ5Lr0&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-        date="Mar 16, 2020"
-        timeToConsume="6 min read"
-        image="https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-1.2.1&ixqx=QWrkbJ5Lr0&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80"
-      />
-      <ResourceLink
-        type="Video"
-        title="How to use search engine optimization to drive sales"
-        summary="Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit facilis asperiores porro quaerat doloribus, eveniet dolore. Adipisci tempora aut inventore optio animi., tempore temporibus quo laudantium."
-        author="Brenna Goyette"
-        authorImage="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixqx=QWrkbJ5Lr0&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-        date="Mar 16, 2020"
-        timeToConsume="6 min read"
-        image="https://images.unsplash.com/photo-1547586696-ea22b4d4235d?ixlib=rb-1.2.1&ixqx=QWrkbJ5Lr0&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80"
-      />
-      <ResourceLink
-        type="Case Study"
-        title="Improve your customer experience"
-        summary="Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint harum rerum voluptatem quo recusandae magni placeat saepe molestiae, sed excepturi cumque corporis perferendis hic."
-        author="Daniela Metz"
-        authorImage="https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixqx=QWrkbJ5Lr0&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-        date="Jun 4, 2020"
-        timeToConsume="11 min read"
-        image="https://images.unsplash.com/photo-1492724441997-5dc865305da7?ixlib=rb-1.2.1&ixqx=QWrkbJ5Lr0&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80"
-      />
+      {#each resources as resource, index (resource)}
+          <ResourceLink 
+            {...resource}
+          />
+      {/each}
     </div>
   </div>
 </div>
